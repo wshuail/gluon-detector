d9f1beea2bb09ac4ba3b898e0913804e49ff11db
wangshuailong <wangshuailong@tuputech.com>
2019-09-26 15:49:18 +0800

    revise retinanet loss

--- lib/cus_loss/retinanet.py
+++ lib/cus_loss/retinanet.py
@@ -1,4 +1,6 @@
-from __future__ import absolute_import
+import os
+import sys
+sys.path.insert(0, os.path.expanduser('~/incubator-mxnet/python'))
 from mxnet import gluon
 from mxnet import nd
 from mxnet.gluon.loss import Loss
@@ -8,29 +10,26 @@ __all__ = ['FocalLoss', 'HuberLoss']
 
 class FocalLoss(Loss):
     def __init__(self, num_class, alpha=0.25, gamma=2, huber_rho=0.11,
-                 eps=1e-12, weight=None, batch_axis=0, **kwargs):
+                 debug=False, eps=1e-12, weight=None, batch_axis=0, **kwargs):
         super(FocalLoss, self).__init__(weight, batch_axis, **kwargs)
         self._num_class = num_class
         self._alpha = alpha
         self._gamma = gamma
         self._huber_rho = huber_rho
         self._eps = eps
+        self._debug = debug
 
     def hybrid_forward(self, F, pred, label):
-        # cls loss
+        print ('cls max pred: {}'.format(F.max(pred)))
+        print ('cls sum pred: {}'.format(F.sum(pred)))
+        pred = F.clip(pred, 1e-4, 1-1e-4)
         # print ('pred: {}'.format(pred))
-        print ('max pred: {}'.format(F.max(pred)))
-        print ('min pred: {}'.format(F.min(pred)))
-        print ('mean pred: {}'.format(F.mean(pred)))
-        # TODO
-        # pred = F.clip(pred, 1e-9, 1-1e-9)
-
-        # fake_label = F.where(label>0, F.zeros_like(label), F.ones_like(label)*-1)
-        # label = F.one_hot(label, self._num_class)
 
         # num of positive samples
-        # num_pos = F.sum(label>0, axis=-1, keepdims=True)
-        # num_pos = F.where(num_pos<=0, F.ones_like(num_pos), num_pos) 
+        num_pos = F.where(label>0, F.ones_like(label), F.zeros_like(label))
+        num_pos = F.sum(label>0, axis=0, exclude=True)
+        num_pos = F.where(num_pos>0, num_pos, F.ones_like(num_pos))
+        # print ('cls num_pos: {}'.format(num_pos))
         
         # convert 0 to -1 for one_hot encoding
         # encode coco class to be [0, 79] instead of [1, 80]
@@ -38,21 +37,17 @@ class FocalLoss(Loss):
         label = F.where(label != -1, label-1, label)
         label = F.one_hot(label, self._num_class)
 
-        # mask = F.where(label != -1, F.ones_like(label), F.zeros_like(label))
-
         alpha_factor = F.ones_like(label) * self._alpha
         alpha_factor = F.where(label, alpha_factor, 1.-alpha_factor)
             
         focal_weight = F.where(label, 1.-pred, pred)
         focal_weight = alpha_factor * F.power(focal_weight, self._gamma)
 
-        bce = -(label * F.log(pred+self._eps) + (1.0-label) * F.log(1.0-pred))
+        bce = -(label * F.log(pred) + (1.0-label) * F.log(1.0-pred))
 
-        # loss = focal_weight * bce
-        loss = bce
+        loss = focal_weight * bce
 
-        loss = F.mean(loss, axis=self._batch_axis, exclude=True)  # .reshape((0, 1))
-        # loss = loss/num_pos
+        loss = F.sum(loss, axis=0, exclude=True)/num_pos
 
         return loss
 
@@ -63,19 +58,52 @@ class HuberLoss(Loss):
         self._rho = rho
 
     def hybrid_forward(self, F, pred, label):
+        print ('box max pred: {}'.format(F.max(pred)))
+        print ('box sum pred: {}'.format(F.sum(pred)))
+        DEBUG = False
         # num of positive samples
-        # num_pos = F.sum(label != 0, axis=(1, 2), keepdims=True).reshape((0, -1))/4
-        # num_pos = F.where(num_pos<=0, F.ones_like(num_pos), num_pos) 
-        # box loss
-        # mask = F.where(label==0, F.zeros_like(label), F.ones_like(label))
+        pos = F.where(label != 0, F.ones_like(label), F.zeros_like(label))
+        num_pos = F.sum(pos, axis=-1)
+        num_pos = F.where(num_pos != 0, F.ones_like(num_pos), F.zeros_like(num_pos)) 
+        num_pos = F.sum(num_pos, axis=0, exclude=True)
+        num_pos = F.where(num_pos>0, num_pos, F.ones_like(num_pos))
+        # print ('box num_pos: {}'.format(num_pos))
+        if DEBUG:
+            label_np = label[0, :, :].asnumpy()
+            label_np_sum = label_np.sum(axis=-1)
+            pos_idx = (label_np_sum != 0)
+            pos_label = label_np[pos_idx, :]
+            # print ('pos_label: {}'.format(pos_label))
+            print ('pos: {}'.format(pos[0, :, :].asnumpy()[pos_idx, :]))
+
+        # print ('pred mean: {}'.format(F.mean(pred)))
         loss = F.abs(label - pred)
         loss = F.where(loss > self._rho, loss - 0.5 * self._rho, (0.5 / self._rho) * F.square(loss))
-        # loss = loss * mask
-        loss = F.mean(loss, axis=self._batch_axis, exclude=True)  # .reshape((0, 1))
-        # loss = loss/num_pos
+        loss = loss*pos
+        loss = F.sum(loss, axis=0, exclude=True)/num_pos
 
         return loss
 
+
+if __name__ == '__main__':
+    pred_origin = nd.random.uniform(-1, 1, (1, 5, 10))
+    pred = nd.sigmoid(pred_origin)
+    print ('pred: {}'.format(pred))
+    label = nd.array((0, 1, 10, 0, 6)).reshape((1, 5))
+    criterion = FocalLoss(num_class=10, debug=True)
+    loss = criterion(pred, label)
+
+    import torch
+    label = nd.where(label == 0, nd.ones_like(label)*-1, label)
+    label = nd.where(label != -1, label-1, label)
+    label = nd.one_hot(label, 10)
+    label = torch.from_numpy(label.asnumpy()).float()
+    pred = torch.from_numpy(pred_origin.asnumpy()).float()
+    sys.path.insert(0, os.path.expanduser('~/retinanet-examples/retinanet'))
+    from loss import FocalLoss
+    py_criterion = FocalLoss()
+    loss = py_criterion(pred, label)
+    print ('pytorch loss: {}'.format(loss))
 
 
 
